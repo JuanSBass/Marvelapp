de28fc08449a38d0156c9aa98032d34365da07dc
JuanSBass <juan_santillan20@hotmail.com>
2023-01-10 14:19:43 -0600

    actualizando env

--- App.js
+++ App.js
@@ -1,39 +1,40 @@
-import * as React from 'react';
-import { Text, View, FlatList, Image } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/Ionicons';
+import * as React from "react";
+import { Text, View, FlatList, Image } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
 const WorldScreen = (props) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>This is a {props.name}!</Text>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center", backgroundColor: "black" }}>
+      <Text style={{ color: "white"}}>Esto es {props.name}!</Text>
     </View>
   );
 };
 
 const FlatListBasics = () => {
   return (
-      <FlatList style={{backgroundColor: 'white'}}
-        data={[
-          {key: 'Iron Man', img: require("./assets/IronMan.png")},
-          {key: 'Thor', img: require('./assets/Thor.png')},
-          {key: 'Captain America', img: require('./assets/Captain.png')},
-          {key: 'Hulk', img: require('./assets/Hulk.png')},
-          {key: 'Black Widow', img: require('./assets/BlackWidow.png')},
-          {key: 'Hawkeye', img: require('./assets/Hawkeye.png')}
-        ]}
-        renderItem={({item}) => 
-          <>
-            <Text>{item.key}</Text>
-            <Image source={item.img} />
-          </>
-        }
-      />
+    <FlatList
+      style={{ backgroundColor: "white" }}
+      data={[
+        { key: "Iron Man", img: require("./assets/IronMan.png") },
+        { key: "Thor", img: require("./assets/Thor.png") },
+        { key: "Captain America", img: require("./assets/Captain.png") },
+        { key: "Hulk", img: require("./assets/Hulk.png") },
+        { key: "Black Widow", img: require("./assets/BlackWidow.png") },
+        { key: "Hawkeye", img: require("./assets/Hawkeye.png") },
+      ]}
+      renderItem={({ item }) => (
+        <>
+          <Text>{item.key}</Text>
+          <Image source={item.img} />
+        </>
+      )}
+    />
   );
-}
+};
 
 export default function App() {
   return (
@@ -41,7 +42,7 @@ export default function App() {
       <Tab.Navigator
         initialRouteName="Avengers"
         screenOptions={{
-          "tabBarActiveTintColor": "darkred"
+          tabBarActiveTintColor: "darkred",
         }}
       >
         {/* <Tab.Screen 
@@ -57,33 +58,37 @@ export default function App() {
              )
           }}
         /> */}
-        <Tab.Screen 
-          name="Asgard" 
+        <Tab.Screen
+          name="Asgard"
           options={{
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="home" color={color} size={size} />
-            )
+            ),
           }}
         >
-          {props => <WorldScreen {...props} name="Asgard" />}
+          {(props) => <WorldScreen {...props} name="Asgard" />}
         </Tab.Screen>
-        <Tab.Screen 
-          name="Nidavellir" 
+        <Tab.Screen
+          name="Nidavellir"
           options={{
             tabBarIcon: ({ color, size }) => (
-              <MaterialCommunityIcons name="home-outline" color={color} size={size} />
-            )
+              <MaterialCommunityIcons
+                name="home-outline"
+                color={color}
+                size={size}
+              />
+            ),
           }}
         >
-          {props => <WorldScreen {...props} name="Nidavellir" />}
+          {(props) => <WorldScreen {...props} name="Nidavellir" />}
         </Tab.Screen>
-        <Tab.Screen 
-          name="Avengers" 
-          component={FlatListBasics} 
+        <Tab.Screen
+          name="Avengers"
+          component={FlatListBasics}
           options={{
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="people" color={color} size={size} />
-            )
+            ),
           }}
         />
       </Tab.Navigator>
